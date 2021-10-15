908380e3a86013f9ef2961b4f9e96d61532dbc32
Mathluiz23 <matheus_luizs@hotmail.com>
2021-10-15 17:30:55 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '../components/Button';
 
-class Login extends React.Component {
+class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <div>
+          <input
+            data-testid="email-input"
+            type="email"
+            label="email"
+            placeholder="Informe seu e-mail"
+            required
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            label="password"
+            placeholder="Informe sua senha"
+            required
+          />
+          <Button />
+        </div>
+      </div>
+    );
   }
 }
 
