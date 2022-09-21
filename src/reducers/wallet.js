27e1efac68abd6c5c01e989a26ba5523ba6589f2
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-09-21 19:44:43 -0300

    :sparkles: feat Implementing logic in the reducer, to filter expenses other than the expense selected to be excluded

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,5 +1,5 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import { SET_CURRENCIES, SET_EXPENSES } from '../actions';
+import { SET_CURRENCIES, SET_EXPENSES, REMOVE_EXPENSE } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -8,12 +8,22 @@ const INITIAL_STATE = {
 
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
-  case SET_CURRENCIES:
-    return { ...state, currencies: action.payload };
-  case SET_EXPENSES:
-    return { ...state, expenses: [...state.expenses, action.payload.expenses] };
-  default:
-    return state;
+    case SET_CURRENCIES:
+      return { ...state, currencies: action.payload };
+    case SET_EXPENSES:
+      return {
+        ...state,
+        expenses: [...state.expenses, action.payload.expenses],
+      };
+    case REMOVE_EXPENSE:
+      return {
+        ...state,
+        expenses: [
+          ...state.expenses.filter((expense) => expense.id !== action.payload),
+        ],
+      };
+    default:
+      return state;
   }
 }
 
