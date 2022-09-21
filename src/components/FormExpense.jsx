53f71276afb281c8a0d1a968b4d719b640fb8cb2
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-09-21 19:52:56 -0300

    :recycle: refactor: Importing css file and Implementing new div wrapping contents for better styling with css

--- src/components/FormExpense.jsx
+++ src/components/FormExpense.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getDataCurrencie, getExpense } from '../services/moedaApi';
 import Button from './Button';
+import '../css/FormExpenseStyle.css';
 
 const TAG_TYPE = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
 const PAY_METHOD = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
@@ -52,60 +53,77 @@ class FormExpense extends Component {
     const { value, description } = this.state;
     return (
       <>
-        <label htmlFor="value">
-          Valor:
+        <div className="label-float">
           <input
-            type="text"
+            placeholder=" "
+            type="number"
             id="value"
             name="value"
             value={ value }
             onChange={ this.handleChange }
           />
-        </label>
-        <label htmlFor="description">
-          Descrição:
-          <textarea
+          <label htmlFor="value">Valor</label>
+        </div>
+        <div className="label-float">
+          <input
+            placeholder=" "
             type="text"
             name="description"
             id="description"
             value={ description }
             onChange={ this.handleChange }
           />
-        </label>
-        <label htmlFor="currency">
-          Moeda:
+          <label htmlFor="description">Descrição</label>
+        </div>
+
+        <div className="select-options">
+          <label htmlFor="currency">
+            <span>Moeda</span>
+          </label>
           <select id="currency" name="currency" onChange={ this.handleChange }>
             { currencies.map((currency) => (
               <option value={ currency } key={ currency }>{ currency }</option>)) }
           </select>
-        </label>
-        <label htmlFor="method">
-          Método de pagamento:
+        </div>
+
+        <div className="select-options">
+          <label htmlFor="method">
+            <span>Método de pagamento</span>
+          </label>
           <select id="method" name="method" onChange={ this.handleChange }>
             { PAY_METHOD.map((pay) => (
               <option value={ pay } key={ pay }>{ pay }</option>)) }
           </select>
-        </label>
-        <label htmlFor="tag">
-          Tag:
+        </div>
+
+        <div className="select-options">
+          <label htmlFor="tag">
+            <span>Tag</span>
+          </label>
           <select id="tag" name="tag" onChange={ this.handleChange }>
             { TAG_TYPE.map((tag) => (
               <option value={ tag } key={ tag }>{ tag }</option>)) }
           </select>
-        </label>
+        </div>
       </>
     );
   }
 
   render() {
     return (
-      <form>
-        {this.formsDespesas()}
-        <Button
-          label="Adicionar despesa"
-          onClick={ this.handleClick }
-        />
-      </form>
+      <>
+        <form className="inputs-form">
+          {this.formsDespesas()}
+          <div>
+            <Button
+              className="button-add"
+              label="Adicionar despesa"
+              onClick={ this.handleClick }
+            />
+          </div>
+        </form>
+        <hr/>
+      </>
     );
   }
 }
