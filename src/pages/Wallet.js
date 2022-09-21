7e07cc30fedfc1e04cf97c5ba78ec28a47a18e2e
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-09-21 19:55:54 -0300

    :sparkles: feat Implementing icon to delete expense, creating logic delete button e implementing action to remove itens according to id

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,58 +1,88 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { MdDeleteForever } from 'react-icons/md';
 import Header from '../components/Header';
 import FormExpense from '../components/FormExpense';
-import Button from '../components/Button';
+import { removeExpense as removeExpenseAction } from '../actions';
+import '../css/FormExpenseStyle.css';
 
 const CHARACTHER_TABLE = [
-  'Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda', 'Câmbio utilizado',
-  'Valor convertido', 'Moeda de conversão', 'Editar/Excluir',
+  'Descrição',
+  'Tag',
+  'Método de pagamento',
+  'Valor',
+  'Moeda',
+  'Câmbio utilizado',
+  'Valor convertido',
+  'Moeda de conversão',
+  'Editar/Excluir',
 ];
 
 class Wallet extends Component {
+  constructor() {
+    super();
+
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+
+  handleDeleteClick(id) {
+    const { removeExpense } = this.props;
+    removeExpense(id);
+  }
+
   render() {
     const { expenses } = this.props;
+    console.log(expenses);
     return (
-      <div>
+      <>
         <Header />
         <FormExpense />
-        {/* tabela */}
-        <table>
-          <thead>
-            <tr>
-              { CHARACTHER_TABLE.map((item) => (
-                <th key={ item }>{ item }</th>)) }
-            </tr>
-          </thead>
-          <tbody>
-            { expenses && expenses.map(
-              ({ id, description, tag, method, value, currency, exchangeRates }) => (
-                <tr key={ id }>
-                  <td>{ description }</td>
-                  <td>{ tag }</td>
-                  <td>{ method }</td>
-                  <td>{ value }</td>
-                  <td>{ (exchangeRates[currency].name).split('/')[0] }</td>
-                  <td>{ (Number(exchangeRates[currency].ask)).toFixed(2) }</td>
-                  <td>
-                    { (value * exchangeRates[currency].ask).toFixed(2)}
-                  </td>
-                  <td>Real</td>
-                  <td>
-                    <Button
-                      label="Delete"
-                      type="button"
-                      data-testid="delete-btn"
-                      name="delete-btn"
-                    />
-                  </td>
-                </tr>
-              ),
-            )}
-          </tbody>
-        </table>
-      </div>
+        <div className="container-table">
+          <table>
+            <thead>
+              <tr>
+                {CHARACTHER_TABLE.map((item) => (
+                  <th key={ item }>{item}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {expenses &&
+                expenses.map(
+                  ({
+                    id,
+                    description,
+                    tag,
+                    method,
+                    value,
+                    currency,
+                    exchangeRates,
+                  }) => (
+                    <tr key={ id }>
+                      <td>{description}</td>
+                      <td>{tag}</td>
+                      <td>{method}</td>
+                      <td>{value}</td>
+                      <td>{exchangeRates[currency].name.split('/')[0]}</td>
+                      <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                      <td>
+                        {(value * exchangeRates[currency].ask).toFixed(2)}
+                      </td>
+                      <td>Real</td>
+                      <td>
+                        <MdDeleteForever
+                          onClick={() => this.handleDeleteClick(id)}
+                          className="delete-icon"
+                        />
+                      </td>
+                    </tr>
+                  )
+                )}
+            </tbody>
+          </table>
+        </div>
+      </>
     );
   }
 }
@@ -61,10 +91,15 @@ Wallet.propTypes = {
   expenses: PropTypes.shape({
     map: PropTypes.func,
   }).isRequired,
+  removeExpense: PropTypes.objectOf.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,
 });
 
-export default connect(mapStateToProps, null)(Wallet);
+const mapDispatchToProps = (dispatch) => ({
+  removeExpense: (id) => dispatch(removeExpenseAction(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
